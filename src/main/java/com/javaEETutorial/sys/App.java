26457daccd7bd3a6d8c49a7f4528624a4d0d8c7e
simonwandera <simonwandera12@gmail.com>
2022-09-25 17:44:47 +0300

    -Initialixe system

---
+++ src/main/java/com/javaEETutorial/sys/App.java
@@ -0,0 +1,16 @@
+package com.javaEETutorial.sys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	List<String> animals = new ArrayList<String>();
+	animals.add("monkey");
+	animals.add("Elephant");
+        System.out.println(animals);
+    }
+}
